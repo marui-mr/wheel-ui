ede64f156fafc2ac56d2a8eb6604f61f12dcaebf
marui-mr <ma1271968407@163.com>
2021-08-11 19:31:02 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Ma />
+</template>
+
+<script>
+import Ma from "./components/Ma.vue";
+
+export default {
+  name: "App",
+  components: {
+    Ma,
+  },
+};
+</script>
