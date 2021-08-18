dd719c55b57e68cea0711c22db72eb152dd9b08b
marui-mr <ma1271968407@163.com>
2021-08-18 17:16:40 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,18 @@
 <template>
-  <button>
+  <button class="wheel-button" :class="`theme-${theme}`">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
