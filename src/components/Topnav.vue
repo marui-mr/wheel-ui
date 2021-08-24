323a5242f88710480f98cd0a73eb8876d3005673
marui-mr <ma1271968407@163.com>
2021-08-24 15:30:11 +0800

    从 iconfont.cn 引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-ui"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -42,6 +46,12 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    display: flex;
+    align-items: center;
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,31 @@
   <div class="topnavAndBanner">
     <Topnav />
     <div class="banner">
-      <h1>Wheel UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
-      <p class="actions">
+      <div class="banner-title-wrapper">
+        <svg class="icon">
+          <use xlink:href="#icon-ui"></use>
+        </svg>
+        <div class="banner-title">
+          <h1>Wheel UI</h1>
+          <h2>一个厉害的 UI 框架</h2>
+        </div>
+      </div>
+      <div class="actions">
         <a href="https://github.com">GitHub</a>
         <router-link to="/doc">开始</router-link>
-      </p>
+      </div>
     </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light"></use>
+    </svg>
   </div>
 </template>
 
@@ -27,17 +45,39 @@ export default {
     rgba(195, 241, 252, 1) 100%
   );
 }
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
-  color: #0a8b9f;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  &-title {
+    color: #0a8b9f;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    &-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      > svg {
+        width: 72px;
+        height: 72px;
+        margin-right: 8px;
+      }
+    }
+  }
   > .actions {
-    padding: 8px 0;
+    padding: 16px 0;
     a {
-      margin: 0 8px;
+      margin: 0 48px;
       background: #102e46;
       display: inline-block;
       padding: 8px 24px;
