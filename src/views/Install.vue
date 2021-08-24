e07abc89801ed358dd82091bd1a77e2c4e95fa4c
marui-mr <ma1271968407@163.com>
2021-08-24 20:26:10 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>安装</div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install wheel-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add wheel-ui</code></pre>
+    <p>下一节：<a href="#/doc/start">开始使用</a></p>
+  </article>
 </template>
