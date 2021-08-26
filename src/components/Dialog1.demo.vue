ea0c1c72ebf7b4a22ef72517a4be3d602921e8ae
marui-mr <ma1271968407@163.com>
2021-08-26 11:20:46 +0800

    改造 DialogDemo

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,34 @@
+<demo>常规用法</demo>
+
+<template>
+  <Button @click="toggle">打开对话框</Button>
+  <Dialog v-model:visible="bool" :closeOnClickOverlay="false" :ok="ok">
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+    <template v-slot:content>
+      <strong>你好</strong>
+      <div>Hi</div>
+    </template>
+  </Dialog>
+</template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
+
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const bool = ref(false);
+    const toggle = () => {
+      bool.value = !bool.value;
+    };
+    const ok = () => {
+      return false;
+    };
+    return { bool, toggle, ok };
+  },
+};
+</script>
---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,27 @@
+<demo>一键打开</demo>
+
+<template>
+  <Button @click="showDialog">打开对话框</Button>
+</template>
+
+<script lang="ts">
+import { h } from "vue";
+import Button from "../lib/Button.vue";
+import { openDialog } from "../lib/openDialog";
+
+export default {
+  components: { Button },
+  setup() {
+    const showDialog = () => {
+      openDialog({
+        title: h("strong", {}, "标题"),
+        content: "你好",
+        ok: () => {
+          return false;
+        },
+      });
+    };
+    return { showDialog };
+  },
+};
+</script>
